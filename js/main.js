3b9d8a65bd117525fe6366c2468be5d5c1006a56
Golokhvastov Dmitry <dargon89@mail.ru>
2019-11-12 09:49:14 +0700

    Написал функцию создания карточки объявления на карте и функцию показа этой карточки

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ var CHECK_OUTS = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
+var TYPES_RUS = ['Дворец', 'Квартира', 'Дом', 'Бунгало'];
 
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -63,7 +64,7 @@ var getPin = function (avatarNumber) {
       avatar: 'img/avatars/user0' + avatarNumber + '.png'
     },
     offer: {
-      title: 'заголовок предложения' + avatarNumber,
+      title: 'заголовок предложения ' + avatarNumber,
       address: locationX + ', ' + locationY,
       price: getRandomInt(PRICE_MIN, PRICE_MAX),
       type: TYPES[getRandomInt(0, TYPES.length - 1)],
@@ -72,7 +73,7 @@ var getPin = function (avatarNumber) {
       checkin: CHECK_INS[getRandomInt(0, CHECK_INS.length - 1)],
       checkout: CHECK_OUTS[getRandomInt(0, CHECK_OUTS.length - 1)],
       features: getRandomArrayPart(FEATURES),
-      description: 'строка с описанием',
+      description: 'строка с описанием ' + avatarNumber,
       photos: getRandomArrayPart(PHOTOS)
     },
     location: {
@@ -109,6 +110,62 @@ var renderSimilarPins = function (list, template, pins) {
   list.appendChild(fragment);
 };
 
+var getTypeRus = function (type) {
+  var typeRus = '';
+  for (var i = 0; i < TYPES.length; i++) {
+    if (type == TYPES[i]) {
+      typeRus = TYPES_RUS[i];
+      break;
+    }
+  }
+  return typeRus;
+};
+
+var removeExcessiveFeatures = function (cardElement, features) {
+  for (var i = 0; i < FEATURES.length; i++) {
+    if (!features.includes(FEATURES[i])) {
+      cardElement.querySelector('.popup__feature--' + FEATURES[i]).remove();
+    }
+  }
+}
+
+var createCardPhotos = function (cardElement, photos) {
+  if (photos.length == 0) {
+    cardElement.querySelector('.popup__photo').remove();
+  } else {
+    cardElement.querySelector('.popup__photo').src = photos[0];
+    if (photos.length > 1) {
+      for (var i = 1; i < photos.length; i++) {
+        var popupPhoto = cardElement.querySelector('.popup__photo').cloneNode(true);
+        popupPhoto.src = photos[i];
+        cardElement.querySelector('.popup__photos').appendChild(popupPhoto);
+      }
+    }
+  }
+};
+
+var createCardElement = function (template, card) {
+  var cardElement = template.content.cloneNode(true);
+  cardElement.querySelector('.popup__title').textContent = card.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = card.offer.price + '₽/ночь';
+  cardElement.querySelector('.popup__type').textContent = getTypeRus(card.offer.type);
+  cardElement.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
+  removeExcessiveFeatures(cardElement, card.offer.features);
+  cardElement.querySelector('.popup__description').textContent = card.offer.description;
+  createCardPhotos(cardElement, card.offer.photos);
+  cardElement.querySelector('.popup__avatar').src = card.author.avatar;
+
+  return cardElement;
+};
+
+var renderCard = function (list, template, card) {
+  var fragment = document.createDocumentFragment();
+  fragment.appendChild(createCardElement(template, card));
+  list.insertBefore(fragment, document.querySelector('.map__filters-container'));
+};
+
 var similarPins = getSimilarPins(PIN_AMOUNT);
 
 var map = document.querySelector('.map');
@@ -118,3 +175,7 @@ var mapPinsList = document.querySelector('.map__pins');
 var similarPinTemplate = document.querySelector('#pin');
 
 renderSimilarPins(mapPinsList, similarPinTemplate, similarPins);
+
+var cardTemplate = document.querySelector('#card');
+
+renderCard(map, cardTemplate, similarPins[0]);
