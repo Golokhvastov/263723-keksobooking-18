8e87a7146c4fe5c7a013151b58771bb96870f7c3
Golokhvastov Dmitry <dargon89@mail.ru>
2019-11-14 14:55:09 +0700

    Заменил new Map() на объект

--- js/main.js
+++ js/main.js
@@ -19,11 +19,12 @@ var CHECK_OUTS = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = 3;
 
-var TYPES_RUS = new Map();
-TYPES_RUS.set('bungalo', 'Бунгало')
-          .set('flat', 'Квартира')
-          .set('house', 'Дом')
-          .set('palace', 'Дворец');
+var TYPES_RUS = {
+  'bungalo': 'Бунгало',
+  'flat': 'Квартира',
+  'house': 'Дом',
+  'palace': 'Дворец'
+}
 
 var ENTER_KEYCODE = 13;
 
@@ -148,7 +149,7 @@ var createCardElement = function (template, card) {
   cardElement.querySelector('.popup__title').textContent = card.offer.title;
   cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
   cardElement.querySelector('.popup__text--price').textContent = card.offer.price + '₽/ночь';
-  cardElement.querySelector('.popup__type').textContent = TYPES_RUS.get(card.offer.type);
+  cardElement.querySelector('.popup__type').textContent = TYPES_RUS[card.offer.type];
   cardElement.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
   cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
   removeExcessiveFeatures(cardElement, card.offer.features);
